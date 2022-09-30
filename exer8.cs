419beeb416770fcb5b57672865cd2ed6b48c3f15
v_seo@list.ru <your_github_email>
2022-09-30 15:09:32 +0300

    Задача8

---
+++ exer8.cs
@@ -0,0 +1,17 @@
+		void divByTwo (int a)
+			
+		  {	
+			int b = 0;
+			do{
+			b++; 
+			if (b % 2 == 0)	
+			Console.WriteLine(b);
+			}
+			while (b<a);
+
+	      }	
+		int numA = 5;
+		divByTwo(numA);
+		
+		int numB = 8;
+		divByTwo(numB);
