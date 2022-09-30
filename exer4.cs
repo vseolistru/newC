0b6925aa774112a784a508a8eae75651d7a077f7
v_seo@list.ru <your_github_email>
2022-09-30 15:09:01 +0300

    Задача4

---
+++ exer4.cs
@@ -0,0 +1,28 @@
+		void max (int a, int b, int c)
+			
+		  {
+			if (a > b && a > c)
+			{Console.WriteLine(a);}
+		
+			if(b > a && b > c)
+			{Console.WriteLine(b);}
+			
+			if(c > a && c > b)
+			{Console.WriteLine(c);}
+		
+	    	}
+		
+		 int numA = 2;
+		 int numB = 3;
+		 int numC = 7;
+		 max(numA, numB, numC);
+		
+		 int numD = 44;
+		 int numE = 5;
+		 int numF = 78;
+		 max(numD, numE, numF);
+		
+		 int numO = 22;
+		 int numP = 3;
+		 int numR = 9;
+		 max(numO, numP, numR);
