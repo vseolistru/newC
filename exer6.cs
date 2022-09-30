92d02338181c4450be5bf387791cf0ee057e1c1b
v_seo@list.ru <your_github_email>
2022-09-30 15:09:23 +0300

    Задача6

---
+++ exer6.cs
@@ -0,0 +1,16 @@
+		void div (int a)
+			
+		  {			
+			if (a % 2 == 0)
+			{Console.WriteLine("true");}	
+			else 
+			{Console.WriteLine("false");}
+		
+	    	}
+		
+		 int numA = 4;
+		 div(numA);
+		 int numB = 3;
+		 div(numB);
+		 int numC = 7;
+		 div(numC);
